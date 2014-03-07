eda2e018bc864ea169ae0e8176bc1537b7c86acf
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-03-07 15:39:32 -0600

    symlink settings on deploy; real schedule.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,12 +10,17 @@ set :whenever_command, "bundle exec whenever"
 
 ssh_options[:forward_agent] = true
 
-role :app, 'deploy@joeyschoblaska.com'
+role :app, "deploy@joeyschoblaska.com"
 
 namespace :deploy do
   task :update_crontab, :roles => :app do
     run "cd #{release_path} && bundle exec whenever whenever --update-crontab #{application}"
   end
+
+  task :symlink_settings, :roles => :app do
+    run "ln -nfs #{shared_path}/settings.yml #{latest_release}/settings.yml"
+  end
 end
 
-after 'deploy:update_code', 'deploy:update_crontab'
+after "deploy:update_code", "deploy:update_crontab"
+after "deploy:update_code", "deploy:symlink_settings"
---
+++ config/schedule.rb
@@ -0,0 +1,8 @@
+job_type :rake, "cd :path && bundle exec rake :task --trace --silent :output"
+
+set :job_template, nil
+set :output, "~/grams_cron.log"
+
+every 1.hour do
+  rake "delete_old_posts"
+end
