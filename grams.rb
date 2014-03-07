93364c3b372614f6d510c5f29d36ab1dc871d1e0
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-03-07 14:02:34 -0600

    receive and process instagram updates

--- grams.rb
+++ grams.rb
@@ -4,4 +4,12 @@ class Grams < Sinatra::Base
   get "/realtime" do
     params["hub.verify_token"] == Grams::Settings[:instagram_client_secret] ? params["hub.challenge"] : "No thank you"
   end
+
+  post "/realtime" do
+    JSON.parse(request.body).each do |update|
+      Grams::Post.create_from_update(update)
+    end
+
+    status 200
+  end
 end
--- grams/post.rb
+++ grams/post.rb
@@ -1,5 +1,17 @@
 class Grams < Sinatra::Base
   class Post
     include Mongoid::Document
+
+    def self.create_from_update(update)
+      Instagram.geography_recent_media(update["object_id"]).each do |post|
+        unless where(:instagram_id => post["id"]).first
+          create!({
+            :instagram_id => post["id"],
+            :link => post["link"],
+            :likes => post["likes"]["count"]
+          })
+        end
+      end
+    end
   end
 end
